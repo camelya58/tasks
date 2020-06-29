87df8a0b1a59f1321fd81b40e040ed8ade03f102
Kamila M <camelya@mail.ru>
2020-06-29 16:54:02 +0300

    add the methods of class Files and Path such as walkFileTree

---
+++ src/Files_Path_methods/CountFilesAndFolders.java
@@ -0,0 +1,53 @@
+package Files_Path_methods;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class Files.CountFilesAndFolders contains utilite Files.walkFileTree.
+ *
+ * @author Kamila Meshcheryakova
+ * created 26.06.2020
+ */
+public class CountFilesAndFolders {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine(); // "/home/camelya/IdeaProjects/save"
+        reader.close();
+
+        Path pathDirectory = Paths.get(line);
+        if (!Files.isDirectory(pathDirectory)) {
+            System.out.println(pathDirectory + " - is not a folder");
+        } else {
+            SearchFileVisitor searchFileVisitor = new SearchFileVisitor();
+            Files.walkFileTree(pathDirectory, searchFileVisitor);
+            List<Path> foundFiles = searchFileVisitor.foundFiles;
+            List<Path> foundFolders = searchFileVisitor.foundFolders;
+            System.out.println("Total folders - " + (foundFolders.size()-1));
+            System.out.println("Total files - " + foundFiles.size());
+            System.out.println("Total size - " + searchFileVisitor.totalSize);
+        }
+    }
+    static class SearchFileVisitor extends SimpleFileVisitor<Path> {
+
+        private final List<Path> foundFiles = new ArrayList<>();
+        private final List<Path> foundFolders = new ArrayList<>();
+        private int totalSize;
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+            foundFolders.add(dir); // it counts all folders including the start folder
+            return FileVisitResult.CONTINUE;
+        }
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            foundFiles.add(file);
+            totalSize += Files.size(file);
+            return FileVisitResult.CONTINUE;
+        }
+    }
+}
---
+++ src/Files_Path_methods/ReadFromUrl.java
@@ -0,0 +1,43 @@
+package Files_Path_methods;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * Class Files_Path.ReadFromUrl
+ *
+ * @author Kamila Meshcheryakova
+ * created by 29.06.2020
+ */
+public class ReadFromUrl {
+    public static void main(String[] args) throws IOException {
+        Path passwords = downloadFile("https://javarush.ru/testdata/secretPasswords.txt",
+                Paths.get("/home/camelya/IdeaProjects/JavaRushTasks"));
+
+        for (String line : Files.readAllLines(passwords)) {
+            System.out.println(line);
+        }
+    }
+
+    public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
+        URL url = new URL(urlString);
+        InputStream is = url.openStream();
+        Path fileName = Path.of(urlString).getFileName();
+        Path tempFile = Files.createTempFile("temp-", ".tmp");
+        try {
+            Files.copy(is, tempFile, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            System.out.println("A download error has occurred.");
+        }
+        is.close();
+        Path resultPath = downloadDirectory.resolve(fileName);
+        Files.move(tempFile, resultPath, StandardCopyOption.REPLACE_EXISTING);
+        return resultPath;
+        // implement this method
+    }
+}
