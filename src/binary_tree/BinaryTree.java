854c85e76f55cc355ac9304d1e364d4bc59ba619
Kamila M <camelya@mail.ru>
2020-07-10 15:15:32 +0300

    add changes in the method remove for BinaryTree

--- src/binary_tree/BinaryTree.java
+++ src/binary_tree/BinaryTree.java
@@ -14,8 +14,8 @@ import java.util.List;
  * created by 08.07.2020
  */
 public class BinaryTree extends AbstractList<String> implements Cloneable, Serializable {
-    private static final ArrayList<Entry<String>> lists = new ArrayList<>();
-    private static int size;
+    private final ArrayList<Entry<String>> lists = new ArrayList<>();
+    private int size;
 
     Entry<String> root;
 
@@ -42,7 +42,7 @@ public class BinaryTree extends AbstractList<String> implements Cloneable, Seria
     public boolean add(String elementName) {
         boolean check = false;
         Entry<String> currentElement = new BinaryTree.Entry<>(elementName);
-        // Case 1. If you want to return the opportunity to have children if you can't add new element to anybody
+        // Method 1. If you want to return the opportunity to have children if you can't add new element to anybody
 //        if (lists.stream().noneMatch(Entry::isAvailableToAddChildren)) {
 //            if (lists.size() == 1) {
 //                lists.get(0).isAvailableToBeParent(lists.get(0));
@@ -111,10 +111,7 @@ public class BinaryTree extends AbstractList<String> implements Cloneable, Seria
             currentElem.parent.rightChild = null;
         }
 
-        // Case 2. If you want to return the opportunity to have children after removing existing children
-        //  currentElem.parent.isAvailableToBeParent(currentElem.parent);
-
-        // Another way of Case 1.
+        // Method 2.
         if (currentElem.parent.leftChild == null && currentElem.parent.rightChild == null &&
                 !currentElem.parent.availableToAddLeftChildren && !currentElem.parent.availableToAddRightChildren)
             currentElem.parent.isAvailableToBeParent(currentElem.parent);
@@ -189,23 +186,48 @@ public class BinaryTree extends AbstractList<String> implements Cloneable, Seria
         System.out.println("The parent of the child 20 is  " + ((BinaryTree) list).getParent("20"));
 
         list.remove("3");
-        System.out.println("The expected parent is null. The actual parent is " + ((BinaryTree) list).getParent("8"));
+        System.out.println("The entry 3 is deleted");
+        System.out.println("The parent of the child 8 is " + ((BinaryTree) list).getParent("8"));
 
         list.add("16");
-        System.out.println("The expected parent is 9. The actual parent is " + ((BinaryTree) list).getParent("16"));
+        System.out.println("The parent of the child 16 is " + ((BinaryTree) list).getParent("16"));
 
         list.remove("4");
+        System.out.println("The entry 4 is deleted");
         list.remove("5");
+        System.out.println("The entry 5 is deleted");
         list.remove("6");
+        System.out.println("The entry 6 is deleted");
         System.out.println("Expected: true. Actual: " + list.add("20"));
-        System.out.println("The expected parent is 1. The actual parent is " + ((BinaryTree) list).getParent("20"));
+        System.out.println("The parent of the child 20 is " + ((BinaryTree) list).getParent("20"));
 
         list.remove("1");
+        System.out.println("The entry 1 is deleted");
         list.remove("2");
+        System.out.println("The entry 2 is deleted");
         System.out.println("The list size is " + list.size());
         System.out.println("Expected: true. Actual: " + list.add("21"));
-        System.out.println("The expected parent is 0. The actual parent is " + ((BinaryTree) list).getParent("21"));
+        System.out.println("The parent of the child 21 is " + ((BinaryTree) list).getParent("21"));
+        System.out.println("The list size is " + list.size());
     }
-}
+} /* Output:
+The list size is 15
+The parent of the child 8 is 3
+The parent of the child 20 is  nobody, because there isn't such element as 20
+The entry 3 is deleted
+The parent of the child 8 is nobody, because there isn't such element as 8
+The parent of the child 16 is 9
+The entry 4 is deleted
+The entry 5 is deleted
+The entry 6 is deleted
+Expected: true. Actual: true
+The parent of the child 20 is 1
+The entry 1 is deleted
+The entry 2 is deleted
+The list size is 0
+Expected: true. Actual: true
+The parent of the child 21 is 0
+The list size is 1
+*/
 
 
