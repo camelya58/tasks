ce0144e16579709c3b581ad6d42f356285d2e56f
Kamila M <camelya@mail.ru>
2020-10-26 10:29:21 +0300

    add example of pattern adapter

---
+++ src/paterns/adapter/Time.java
@@ -0,0 +1,21 @@
+package paterns.adapter;
+
+/**
+ * Interface Time
+ *
+ * @author Kamila Meshcheryakova
+ * created by 26.10.2020
+ */
+public interface Time {
+
+    // methods allows to get current time
+    int getSeconds();
+    int getMinutes();
+    int getHours();
+}
+
+interface TotalTime {
+
+    // method allows to get quantity of seconds after midnight
+    int getTotalSeconds();
+}
